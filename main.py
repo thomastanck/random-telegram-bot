f7a16ce4e675a802064bb83b9b76e3ea297c6956
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2018-11-28 19:43:11 +0000

    Add command to fetch module info

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import subprocess
 import logging
 import wikipediaapi
 import json
+import requests
+import datetime
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
@@ -102,6 +104,84 @@ def wikipedia(bot, update):
 
 {page.fullurl}''')
 
+def nusmods(bot, update):
+    _, query, *rest = update.message.text.split()
+    update.message.reply_markdown(get_mod_info(query, *rest))
+
+def get_mod_info(module_code, *options):
+    r = requests.get(f'http://api.nusmods.com/2018-2019/modules/{module_code.upper()}/index.json')
+    modinfo = r.json()
+
+    newline = '\n'
+
+    moddesc = modinfo.get('ModuleDescription', '').strip()
+    semesters = [x.get('Semester', 0) for x in modinfo.get('History', [])]
+    hasexam = any('ExamDate' in x for x in modinfo.get('History', []))
+
+    title = f'''*{modinfo.get('ModuleCode', module_code.upper()).strip()} {modinfo.get('ModuleTitle', '').strip()}*'''
+    mc = f'({modinfo["ModuleCredit"]} MC)''' if 'ModuleCredit' in modinfo else ''
+    sems = ' ∙ '.join(f'Sem {x}' for x in semesters)
+
+    examtimes = '\n'.join(
+            f'''Semester {x['Semester']}: ''' + (
+                f'''{datetime.datetime.strptime(x['ExamDate'], '%Y-%m-%dT%H:%M+0800').strftime('%d %b %Y %H:%M')}'''
+                if 'ExamDate' in x
+                else 'No Exam')
+            for x in modinfo.get('History', []))
+
+    header = f'''{title} {mc}'''.strip()
+
+    if not options and 'exam' not in options:
+        header += '\n'
+        header += f'''{sems} {'' if hasexam else '-- *No exam*'}'''.strip()
+        header += '\n'
+
+    data = {
+        'short': min(
+            moddesc[:moddesc.find('. ')] + '.',
+            moddesc[:moddesc.find('! ')] + '!',
+            moddesc[:moddesc.find('? ')] + '?'),
+
+        'desc': f'''
+*Description*
+{moddesc}
+            '''.rstrip(),
+
+        'prereq': f'''
+*Prerequisites*
+{modinfo.get('Prerequisite', 'Nil').strip()}
+            '''.rstrip(),
+
+        'exam': f'''
+*Exam*
+{examtimes}
+            '''.rstrip(),
+    }
+
+    smalldata = {
+        'short': min(
+            moddesc[:moddesc.find('. ')] + '.',
+            moddesc[:moddesc.find('! ')] + '!',
+            moddesc[:moddesc.find('? ')] + '?'),
+        'desc': moddesc,
+        'prereq': modinfo.get('Prerequisite', 'Nil').strip(),
+        'exam': examtimes,
+    }
+
+    options = options or ['short', 'prereq']
+    content = '\n'.join(data[x] for x in options)
+
+    if len(options) == 1:
+        return f'''
+{header}
+{smalldata[options[0]]}
+            '''.strip()
+    else:
+        return f'''
+{header}
+{content}
+            '''.strip()
+
 modifiers = {
         'random': lambda s: ''.join(x.lower() if random.random() < 0.5 else x.upper() for x in s),
         'delete': lambda s: ''.join(x for x in s if random.random() < 0.8),
@@ -138,6 +218,7 @@ def main():
     # updater.dispatcher.add_handler(CommandHandler('eval', evaluatescheme))
     updater.dispatcher.add_handler(CommandHandler('wiki', wiki))
     updater.dispatcher.add_handler(CommandHandler('wikipedia', wikipedia))
+    updater.dispatcher.add_handler(CommandHandler('mod', nusmods))
 
     updater.dispatcher.add_handler(MessageHandler(Filters.photo, photocmd))
 
